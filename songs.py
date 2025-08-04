2afda407986cd56d4c6f1a43c0841d5a7b4dbd9a
Krishthick <79474339+KRISHTHICK@users.noreply.github.com>
2025-08-04 21:48:52 +0530

    Create songs.py

---
+++ songs.py
@@ -0,0 +1,22 @@
+mood_to_songs = {
+    "happy": [
+        "Happy – Pharrell Williams",
+        "Can't Stop the Feeling – Justin Timberlake",
+        "Walking on Sunshine – Katrina & The Waves"
+    ],
+    "sad": [
+        "Someone Like You – Adele",
+        "Fix You – Coldplay",
+        "Let Her Go – Passenger"
+    ],
+    "relaxed": [
+        "Weightless – Marconi Union",
+        "Clair de Lune – Debussy",
+        "Better Together – Jack Johnson"
+    ],
+    "angry": [
+        "Stronger – Kanye West",
+        "In the End – Linkin Park",
+        "Break Stuff – Limp Bizkit"
+    ]
+}
